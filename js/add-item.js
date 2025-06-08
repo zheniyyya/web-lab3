88c7ea543ff0855e5498c02aa460b201c7fb6a9d
Yevhenia Kravchenko <y.kravchenko@ukma.edu.ua>
2025-06-08 13:42:05 +0300

    Реалізовано додавання товару

---
+++ js/add-item.js
@@ -0,0 +1,38 @@
+const addItemForm = document.querySelector('.add_item_form');
+const itemInput = document.querySelector('.add_item_form .input');
+const itemsContainer = document.querySelector('.item_container');
+
+addItemForm.addEventListener('submit', function(event) {
+    // Запобігаємо перезавантаженню сторінки
+    event.preventDefault();
+
+    const itemName = itemInput.value.trim();
+
+    if (itemName === '') {
+        return;
+    }
+
+    const newItemHTML = `
+        <section class="item">
+            <div class="item_name" contenteditable="true">
+                <p>${itemName}</p>
+            </div>
+            <div class="amount_control">
+                <button class="minus" data-tooltip="Зменшити">-</button>
+                <button class="count_value">1</button>
+                <button class="plus" data-tooltip="Збільшити">+</button>
+            </div>
+            <div class="state_indicator">
+                <button class="bought">Не куплено</button>
+                <button class="cancel" data-tooltip="Видалити">x</button>
+            </div>
+        </section>
+    `;
+
+    itemsContainer.insertAdjacentHTML('beforeend', newItemHTML);
+    // Очищуємо поле вводу
+    itemInput.value = '';
+
+    // Повертаємо курсор в поле вводу
+    itemInput.focus();
+});
