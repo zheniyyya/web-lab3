3d743daf3f7e91704bfa1f47752400e5dfd6fb2f
Yevhenia Kravchenko <y.kravchenko@ukma.edu.ua>
2025-06-08 13:53:38 +0300

    Активовано кнопку видалити

---
+++ js/cancel-button.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const itemsContainer = document.querySelector('.item_container');
+
+    if (itemsContainer) {
+        itemsContainer.addEventListener('click', (event) => {
+
+            if (event.target.classList.contains('cancel')) {
+                const itemToRemove = event.target.closest('.item');
+                if (itemToRemove) {
+                    itemToRemove.remove();
+                }
+            }
+        });
+    }
+});
